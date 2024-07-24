6a6fd01b02be3351534e2a95c6188438053190cf
Carlos Daniel da Silva Alves <69328049+CarlosDanielAlv@users.noreply.github.com>
2024-07-24 11:07:53 -0300

    Add files via upload

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 50;
+    idades[3] = 60;
+    idades[4] = 70;
+
+    Console.WriteLine($"Tamanho do array {idades.Length}");
+
+    int acumulador = 0;
+    foreach (int i in idades)
+    {
+        Console.WriteLine(i);
+        acumulador += i;
+    }
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média =  {media}");
+}
+
+TestaArrayInt();
